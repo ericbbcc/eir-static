4ce593ab4278295a6233beb270c36b609da38d07
luchen <378979705@qq.com>
2015-06-22 19:37:20 +0800

    update version

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -2,5 +2,22 @@
  * Created by float.lu on 6/20/15.
  */
 
-console.log('home');
+$('.eir-nav-collapsed').click(function(){
+        $('.eir-tags-pop').slideToggle();
+});
 
+var options = {
+    title:'是否确认删除？',
+    content:"<a class='btn btn-danger eir-pop-btn'>确定</a><a class='btn btn-default eir-pop-btn'>取消</a>",
+    html:true,
+    template:'<div class="popover" role="tooltip">' +
+    '<div class="arrow">' +
+    '</div>' +
+    '<h3 class="popover-title"></h3>' +
+    '<div class="popover-content">' +
+    '</div>' +
+    '</div>'
+};
+$(function () {
+    $('[data-toggle="popover"]').popover(options)
+})
